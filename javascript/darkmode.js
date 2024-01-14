1992dc0e63fbb900c0b8db7d922ccf0c52762535
llayz46 <layzer@outlook.fr>
2024-01-14 19:45:44 +0100

    Finishing the "homemade" dark mode JS

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -2,16 +2,22 @@ const switcherDay = document.querySelector('.js-light')
 const switcherNight = document.querySelector('.js-dark')
 
 const main = document.querySelectorAll('.js-main')
+const modeSelect = document.querySelector('.js-mode-select')
 const title = document.querySelectorAll('.js-title')
 const content = document.querySelectorAll('.js-content')
+const cards = document.querySelectorAll('.js-cards')
+const cardText = document.querySelectorAll('.js-cards-text')
+const cardBtn = document.querySelectorAll('.js-cards-btn')
 const formBg = document.querySelectorAll('.js-form')
 const formBorder = document.querySelectorAll('.js-form-border')
 
 switcherNight.addEventListener('click', () => {
   switcherDay.classList.remove('active')
-  switcherNight.classList.add('active')
+  switcherNight.classList.add('active-night')
 
   document.body.classList.add('js-body-dark-mode')
+  modeSelect.style.backgroundColor = "#040817"
+
   main.forEach(function(each) {
     each.style.backgroundColor = "#040817"
   })
@@ -24,6 +30,18 @@ switcherNight.addEventListener('click', () => {
     each.style.color = "rgba(255, 255, 255, 0.6)"
   })
 
+  cards.forEach(function(each) {
+    each.style.backgroundColor = "#0e163e"
+  })
+
+  cardText.forEach(function(each) {
+    each.style.color = "#fff"
+  })
+
+  cardBtn.forEach(function(each) {
+    each.style.border ="1px solid #3758f9"
+  })
+
   formBg.forEach(function(each) {
     each.style.backgroundColor = "#0e163e"
   })
@@ -34,9 +52,42 @@ switcherNight.addEventListener('click', () => {
 })
 
 switcherDay.addEventListener('click', () => {
-  switcherNight.classList.remove('active')
+  switcherNight.classList.remove('active-night')
   switcherDay.classList.add('active')
 
   document.body.classList.remove('js-body-dark-mode')
+  modeSelect.style.backgroundColor = ""
+
+  main.forEach(function(each) {
+    each.style.backgroundColor = ""
+  })
+
+  title.forEach(function(each) {
+    each.style.color = ""
+  })
+
+  content.forEach(function(each) {
+    each.style.color = ""
+  })
+
+  cards.forEach(function(each) {
+    each.style.backgroundColor = ""
+  })
+
+  cardText.forEach(function(each) {
+    each.style.color = ""
+  })
+
+  cardBtn.forEach(function(each) {
+    each.style.border =""
+  })
+
+  formBg.forEach(function(each) {
+    each.style.backgroundColor = ""
+  })
+
+  formBorder.forEach(function(each) {
+    each.style.border = ""
+  })
 })
 
