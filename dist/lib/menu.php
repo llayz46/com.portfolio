e15010a6d3cd981252afaa2aeb7312c4cbc892d7
llayz46 <layzer@outlook.fr>
2024-04-25 10:01:52 +0200

    starting: login page

--- dist/lib/menu.php
+++ dist/lib/menu.php
@@ -4,5 +4,6 @@ $mainMenu = [
   'index.php' => ['menu_title' => 'Home', 'head_title' => 'Home - Portfolio', 'head_meta' => 'Welcome to my portfolio! I\'m llayz a junior web developer. Find here my personal and professional projects.', 'exclude' => true],
   'projects.php' => ['menu_title' => 'Projects', 'head_title' => 'Projects - Portfolio', 'head_meta' => 'Here, you will be able to explore all of my projects, both personal and professional, and learn more about my skills and experiences.'],
   'contact.php' => ['menu_title' => 'Contact', 'head_title' => 'Contact - Portfolio', 'head_meta' => 'Feel free to contact me for project estimates, information, or collaboration opportunities.'],
-  'admin.php' => ['menu_title' => 'Admin', 'head_title' => 'Admin - Portfolio', 'head_meta' => 'Espace d\'administration du portfolio.']
+  'admin.php' => ['menu_title' => 'Admin', 'head_title' => 'Admin - Portfolio', 'head_meta' => 'Espace d\'administration du portfolio.'],
+  'login.php' => ['menu_title' => 'Login', 'head_title' => 'Login - Portfolio', 'head_meta' => 'Login to access the admin area.', 'exclude' => true],
 ];
