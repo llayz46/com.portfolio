025d5d7165328a045471a68e4ae3342b524718c9
llayz46 <layzer@outlook.fr>
2024-05-17 20:25:18 +0200

    remove from git index: src folder

--- dist/lib/config.php
+++ dist/lib/config.php
@@ -1,11 +1,17 @@
 <?php
 
-define('_DOMAIN_', getenv('DOMAIN'));
-define('_DB_HOST_', getenv('DB_HOST'));
-define('_DB_NAME_', getenv('DB_NAME'));
-define('_DB_USER_', getenv('DB_USER'));
-define('_DB_PASS_', getenv('DB_PASS'));
-define('_DB_PORT_', getenv('DB_PORT'));
+// define('_DOMAIN_', getenv('DOMAIN'));
+// define('_DB_HOST_', getenv('DB_HOST'));
+// define('_DB_NAME_', getenv('DB_NAME'));
+// define('_DB_USER_', getenv('DB_USER'));
+// define('_DB_PASS_', getenv('DB_PASS'));
+// define('_DB_PORT_', getenv('DB_PORT'));
+
+define('_DOMAIN_', 'portfolio06.local');
+define('_DB_HOST_', 'localhost');
+define('_DB_NAME_', 'portfolio');
+define('_DB_USER_', 'layz');
+define('_DB_PASS_', 'EG--hNJ6lCi_9n]I');
 
 define('_PATH_ASSETS_IMAGES_', '/assets/image/');
 define('_PATH_UPLOADS_PROJECTS_', '/uploads/projects/');
--- dist/lib/pdo.php
+++ dist/lib/pdo.php
@@ -1,10 +1,19 @@
 <?php
 
+// try {
+//   $pdo = new PDO('mysql:host='._DB_HOST_.';port='._DB_PORT_.';dbname='._DB_NAME_.';charset=utf8mb4', _DB_USER_, _DB_PASS_, [
+//     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+//   ]);
+// } catch (PDOException $e) {
+//   die('Une erreur est survenue lors de la connexion à la base de données.');
+// }
+
 try {
-  $pdo = new PDO('mysql:host='._DB_HOST_.';port='._DB_PORT_.';dbname='._DB_NAME_.';charset=utf8mb4', _DB_USER_, _DB_PASS_, [
+  $pdo = new PDO('mysql:host='._DB_HOST_.';dbname='._DB_NAME_.';charset=utf8mb4', _DB_USER_, _DB_PASS_, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-  ]);
+  ]); // Définis le mode d'erreur et de récupération par défaut
 } catch (PDOException $e) {
   die('Une erreur est survenue lors de la connexion à la base de données.');
 }
